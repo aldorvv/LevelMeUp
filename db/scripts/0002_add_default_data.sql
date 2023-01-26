881d6f0dcf8408df98e72086e961f16dae71d037
Aldo Vázquez <aldo@arkangeles.com>
2023-01-25 19:33:57 -0600

    Added default data for mock.

---
+++ db/scripts/0002_add_default_data.sql
@@ -0,0 +1,11 @@
+INSERT INTO skills (skillName) VALUES ("COOKING");
+INSERT INTO skills (skillName) VALUES ("EXCERCISING");
+INSERT INTO skills (skillName) VALUES ("CODING");
+INSERT INTO skills (skillName) VALUES ("READING");
+INSERT INTO skills (skillName) VALUES ("PAINTING");
+
+INSERT INTO priorityLevels (levelName, intValue) VALUES ("HIGHEST", 5);
+INSERT INTO priorityLevels (levelName, intValue) VALUES ("HIGH", 4);
+INSERT INTO priorityLevels (levelName, intValue) VALUES ("MEDIUM", 3);
+INSERT INTO priorityLevels (levelName, intValue) VALUES ("LOW", 2);
+INSERT INTO priorityLevels (levelName, intValue) VALUES ("MEH", 1);
